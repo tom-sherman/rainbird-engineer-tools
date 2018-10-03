db571afb7095f4bc3a009eea88156e2ce2927212
Tom Sherman <the.tomsherman@gmail.com>
2018-10-03 12:41:22 +0100

    Add createFacts tests

--- src/create-facts.ts
+++ src/create-facts.ts
@@ -8,17 +8,12 @@ export async function createFacts () {
   if (!editor) {
       return;
   }
+
   const { startLine, endLine } = getBoundaryLines(editor);
   const range = new Range(
     new Position(startLine, 0),
     new Position(endLine, Number.MAX_SAFE_INTEGER)
   );
-
-  const minIndent = getMinIndent(editor.document.getText(range));
-  const text = editor.document.getText(range)
-    .split('\n')
-    .map(line => line.replace(minIndent, ''))
-    .join('\n');
 
   const relationship = await window.showInputBox({
       placeHolder: 'Relationship name'
@@ -27,11 +22,13 @@ export async function createFacts () {
   if (!relationship) {
     return;
   }
+  const text = editor.document.getText(range);
+  const minIndent = getMinIndent(text);
+  let facts = minIndent ;
 
-  let facts = minIndent;
   for (const [ subject, objects ] of parseFacts(text).entries()) {
     objects.forEach(object => {
-      facts = `${ facts }\n${ minIndent }<relinst subject="${ subject.trim() }" object="${ object.trim() }" rel="${ relationship }" />`;
+      facts = `${ facts }\n${ minIndent }<relinst subject="${ subject }" object="${ object }" rel="${ relationship }" />`;
     });
   }
 
@@ -50,7 +47,12 @@ export async function createFacts () {
  *
  * This will create 3 facts in a Map of Map<subject, object[]>
  */
-function parseFacts (text: string) {
+export function parseFacts (text: string) {
+  const minIndent = getMinIndent(text);
+  text = text.split('\n')
+    .map(line => line.replace(minIndent, ''))
+    .join('\n');
+
   const factMap: Map<string, string[]> = new Map();
   let currentSubject: string = '';
   const reObject = /(?:\t+|(?: {2}))\w+/;
@@ -60,25 +62,25 @@ function parseFacts (text: string) {
       const currentObjects = factMap.get(currentSubject);
 
       if (factMap.has(currentSubject) && typeof currentObjects !== 'undefined') {
-        currentObjects.push(line);
+        currentObjects.push(line.trim());
       } else {
-        factMap.set(currentSubject, [ line ]);
+        factMap.set(currentSubject, [ line.trim() ]);
       }
     } else {
-      currentSubject = line;
+      currentSubject = line.trim();
     }
   });
 
   return factMap;
 }
 
-function getMinIndent (text: string) {
+export function getMinIndent (text: string) {
   let tabIndents: boolean = true;
 
   const nonEmptyLines = text.split('\n').filter(line => line.trim().length > 0);
 
   const lineIndents = nonEmptyLines.map(line => {
-    let matches = line.match(/(\t)|( +)\w+$/);
+    const matches = line.match(/^(?:(\t)|( +))[\w\s]+$/);
 
     if (matches && matches[1]) {
       tabIndents = true;
---
+++ src/test/create-facts.test.ts
@@ -0,0 +1,45 @@
+import * as assert from 'assert';
+import { getMinIndent, parseFacts } from '../create-facts';
+
+suite('Command :: Create facts', function () {
+  test('gets correct min indent', function () {
+    let indentDict = {
+      '0 indent\n    4 spaces\n  2 spaces': '',
+      ' 1 space\n0 space\n    4 spaces': '',
+      '    4 spaces\n      6 spaces': '    ',
+      '\t1 tab\n0 tabs': '',
+      '\t\t2 tabs\n\t1 tab\n0 tabs': '',
+      '\t1 tab\n\t\t2 tabs\n\t\t2 tabs': '\t'
+    };
+
+    for (const [ string, indent ] of Object.entries(indentDict)) {
+      const minIndent = getMinIndent(string);
+      assert.equal(minIndent, indent);
+    }
+  });
+
+  test('parses facts', function () {
+    const facts1 = parseFacts('tom\n'
+                            + '  english\n'
+                            + '  spanish\n'
+                            + 'dave\n'
+                            + '  welsh');
+    const facts2 = parseFacts('  tom\n'
+                            + '    english\n'
+                            + '    spanish\n'
+                            + '  dave\n'
+                            + '    welsh');
+    assert.equal(facts1.size, 2);
+    assert.equal(facts2.size, 2);
+    assert.equal(facts1.get('tom')!.join(' '), 'english spanish');
+    assert.equal(facts2.get('tom')!.join(' '), 'english spanish');
+    assert.equal(facts1.get('dave')!.join(' '), 'welsh');
+    assert.equal(facts2.get('dave')!.join(' '), 'welsh');
+
+    const facts3 = parseFacts('tom\n'
+                            + '  english');
+    assert.equal(facts3.size, 1);
+    assert.equal(facts3.get('tom')!.length, 1);
+    assert.equal(facts3.get('tom')![0], 'english');
+  });
+});
